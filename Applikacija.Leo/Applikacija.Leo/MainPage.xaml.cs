668ae479a66d739fcf87d33bf02b55070c3e6382
Leonid Aerodrom <indovleo59@gmail.com>
2021-07-15 14:30:59 +0200

    1

---
+++ Applikacija.Leo/Applikacija.Leo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Applikacija.Leo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(entUsername.Text == "admin" && entPassword.Text == "admin")
+            {
+                Navigation.PushAsync(new NextPage(entUsername.Text));
+            }
+            else
+            {
+                DisplayAlert("Грешка", "Невалидно. Обидете се повторно!", "Продолжи");
+            }
+        }
+    }
+}
---
+++ Applikacija.Leo/Applikacija.Leo/NextPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Applikacija.Leo
+{
+    public class NextPage : ContentPage
+    {
+        public NextPage(string user)
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Успешна најава, " + user, 
+                        FontSize = Device.GetNamedSize(NamedSize.Header, typeof(Label)), 
+                        FontAttributes = FontAttributes.Bold, 
+                        HorizontalTextAlignment = TextAlignment.Center }
+                }
+            };
+        }
+    }
+}
